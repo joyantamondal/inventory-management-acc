873cb661ce30504bcd52e3468164257f30efe406
Joyanta Mondal <engr.joyantamondal@gmail.com>
2022-09-21 01:30:09 +0600

    Method model get post added

--- app.js
+++ app.js
@@ -7,81 +7,130 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
-// schema design
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name for this product"],
-    trim: true,
-    unique: [true, "Name must be unique"],
-    minlength: [3, "Product name must be at least 3 character"],
-    maxlength: [100, "Product name cann't be greater than 100 character"],
-  },
-  description: {
-    type: String,
-    required: [true, "Description required"],
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: [0, "Price cann't be negative"],
-  },
-  unit: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["kg", "litre", "pcs"],
-      message: "Unit value cann't be {VALUE}, must be kg/litre/pcs",
+// schema design created
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name for this product"],
+      trim: true,
+      unique: [true, "Name must be unique"],
+      minlength: [3, "Product name must be at least 3 character"],
+      maxlength: [100, "Product name cann't be greater than 100 character"],
     },
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "Quantity cann't be negative"],
-    validate: {
-      validator: (value) => {
-        const isInteger = Number.isInteger(value);
-        if (isInteger) {
-          return true;
-        } else {
-          return false;
-        }
+    description: {
+      type: String,
+      required: [true, "Description required"],
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price cann't be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs"],
+        message: "Unit value cann't be {VALUE}, must be kg/litre/pcs",
       },
     },
-    message: "Quantity must be an integer",
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["in-stock", "out-of-stock", "discontinued"],
-      message: "Status cann't be {VALUE}",
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity cann't be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "Quantity must be an integer",
     },
-  },
-  // createdAt: {
-  //   type: Date,
-  //   default: Date.now,
-  // },
-  // updatedAt: {
-  //   type: Date,
-  //   default: Date.now,
-  // },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "Status cann't be {VALUE}",
+      },
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // updatedAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
 
-supplier:{
-type: mongoose.Schema.Types.ObjectId,
-ref: "Supplier",
-},
-categories:[{
-name:{
-type: String,
-required: true,
-},
-_id:mongoose.Schema.Types.ObjectId,
-}]
-},{timestamps:true});
+    // supplier: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Supplier",
+    // },
+    // categories: [
+    //   {
+    //     name: {
+    //       type: String,
+    //       required: true,
+    //     },
+    //     _id: mongoose.Schema.Types.ObjectId,
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+
+// mongoose middleware for saving data : pre/post
+productSchema.pre('save',function(next){
+console.log('Before saving data');
+if(this.quantity===0){
+this.status ='out-of-stock'
+}
+next()
+})
+
+// productSchema.post('save',function(doc,next){
+//   console.log('After saving data');
+//   next()
+//   })
+productSchema.methods.logger=function(){
+console.log(`Data Saved for ${this.name}`)
+}
+// Schema -> Model-> Query
+// model create 
+const Product = mongoose.model("Product", productSchema);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
+});
+
+//posting to database
+app.post("/api/v1/product", async(req, res, next) => {
+  try{
+    // create method 
+    const result = await Product.create(req.body);
+    result.logger()
+    // save  method 
+    // const product = new Product(req.body);
+    // const result= await product.save();
+    res.status(200).json({
+    status:"success",
+    message: "Data inserted successfully",
+    data: result
+    })
+  }
+  catch(error){
+    res.status(400).json({
+   status:"Fail",
+   message:"Data is not inserted",
+   error: error.message
+    })
+  }
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ console.log('Database Connection Successful'.bold.red.bgCyan);
 })
 
 // server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
